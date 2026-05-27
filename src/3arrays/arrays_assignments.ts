0e7ec81f4bb4d176b0121034df06b9a1385ab970
nagarjun-kasu <nagarjunreddykasu1@gmail.com>
2026-05-27 21:42:11 +0530

    ts

--- src/3arrays/arrays_assignments.ts
+++ src/3arrays/arrays_assignments.ts
@@ -1,4 +1,32 @@
 /*
-
+Assignments:
+====================
+1. Write a program to find the largest element in an array.
+const arr = [10, 5, 20, 15, 8];
+2. Write a program to find the smallest element in an array.
+const arr = [10, 5, 20, 15, 8];
+3. Write a program to calculate the average of an array of numbers.
+const arr = [10, 5, 20, 15, 8];
+4. Write a program to reverse an array.
+const arr = [10, 5, 20, 15, 8];
+5. Write a program to check if an array contains a specific element.
+const arr = [10, 5, 20, 15, 8];
+const elementToFind = 15;
+6. Write a program to remove duplicates from an array.
+const arr = [10, 5, 20, 15, 8, 10, 5];
+7. Write a program to sort an array in ascending order.
+const arr = [10, 5, 20, 15, 8];
+8. Write a program to sort an array in descending order.
+const arr = [10, 5, 20, 15, 8];
+9. Write a program to find Sum of Even Numbers in Array.
+const arr = [10, 5, 20, 15, 8];
+10. Write a program to find Sum of Odd Numbers in Array.
+const arr = [10, 5, 20, 15, 8];
+11. Write a program to update first character of each string in an array to uppercase.
+const arr = ['playwright', 'typescript', 'javascript', 'selenium', 'automation'];
+12. Write a program to update the first character of each word in a given string to uppercase and return the modified string.
+const str = "playwright with typescript"; //Playwright With Typescript
+13. Write a program to reverse the each word in a given string and return the modified string.
+const str = "playwright with typescript"; //thgiryalp htiw tpircsepyt
 
 */
